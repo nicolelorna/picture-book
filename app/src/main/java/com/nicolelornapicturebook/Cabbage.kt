b4331dad7be389a6e20cd4e90ff6109eaa2f62ff
nicolelorna <nicolelornah02@gmail.com>
2020-07-27 02:09:14 +0300

    Added 2 Activities

--- app/src/main/java/com/nicolelornapicturebook/Cabbage.kt
+++ app/src/main/java/com/nicolelornapicturebook/Cabbage.kt
@@ -1,11 +1,17 @@
 package com.nicolelornapicturebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cabbage.*
 
 class Cabbage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cabbage)
+
+        to_tomatoe.setOnClickListener {
+            startActivity(Intent(this, Tomatoe::class.java))
+        }
     }
 }
--- app/src/main/java/com/nicolelornapicturebook/Kales.kt
+++ app/src/main/java/com/nicolelornapicturebook/Kales.kt
@@ -1,11 +1,17 @@
 package com.nicolelornapicturebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_kales.*
 
 class Kales : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kales)
+
+        to_cabbage.setOnClickListener {
+            startActivity(Intent(this, Cabbage::class.java))
+        }
     }
 }
