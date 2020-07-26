c956377e259ec249f0cb2def273d0e24ff4f5a3a
nicolelorna <nicolelornah02@gmail.com>
2020-07-27 01:11:35 +0300

    Added MainActivity Content

--- app/src/main/java/com/nicolelornapicturebook/MainActivity.kt
+++ app/src/main/java/com/nicolelornapicturebook/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.nicolelornapicturebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Hiding the toolbar to make the App Look Good
+        supportActionBar!!.hide()
+
+        //Creating A clicklistener to start tour button
+        btn_start_tour.setOnClickListener {
+
+            startActivity(Intent(this, Kales::class.java))
+        }
     }
 }
