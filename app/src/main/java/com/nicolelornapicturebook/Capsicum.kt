9ffb9cd5fdd0d1884f18a3157458d6f45ef1176f
nicolelorna <nicolelornah02@gmail.com>
2020-07-27 02:28:21 +0300

    Added Remaining Activities

--- app/src/main/java/com/nicolelornapicturebook/Capsicum.kt
+++ app/src/main/java/com/nicolelornapicturebook/Capsicum.kt
@@ -1,11 +1,18 @@
 package com.nicolelornapicturebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_capsicum.*
 
 class Capsicum : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_capsicum)
+
+
+        to_carrot.setOnClickListener {
+            startActivity(Intent(this, Carrot::class.java))
+        }
     }
 }
--- app/src/main/java/com/nicolelornapicturebook/Tomatoe.kt
+++ app/src/main/java/com/nicolelornapicturebook/Tomatoe.kt
@@ -1,11 +1,17 @@
 package com.nicolelornapicturebook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_tomatoe.*
 
 class Tomatoe : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tomatoe)
+
+        to_capsicum.setOnClickListener {
+            startActivity(Intent(this, Capsicum::class.java))
+        }
     }
 }
