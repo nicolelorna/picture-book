7fa3cd343a611f3dd8d3ff904a4978e76b1e7127
nicolelorna <nicolelornah02@gmail.com>
2020-07-27 00:41:42 +0300

    Added All Required Activities

---
+++ app/src/main/java/com/nicolelornapicturebook/Cabbage.kt
@@ -0,0 +1,11 @@
+package com.nicolelornapicturebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cabbage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cabbage)
+    }
+}
---
+++ app/src/main/java/com/nicolelornapicturebook/Kales.kt
@@ -0,0 +1,11 @@
+package com.nicolelornapicturebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Kales : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kales)
+    }
+}
